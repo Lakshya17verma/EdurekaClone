961a74bf3744cd9b960d962505f66ed70b90d9e8
Lakshya17verma <lakshmanverma17@gmail.com>
2023-06-10 08:38:36 +0530

    update
    update

---
+++ data.js
@@ -0,0 +1,222 @@
+const categoriesItems = [
+    "Cloud Computing",
+    "DevOps",
+    "BI and Visualization",
+    "Cyber Security",
+    "Data Science",
+    "Programming & Frameworks",
+    "Project Management & Methodology",
+    "software Testing",
+    "big data",
+    "artificial intelligence",
+    "Frontend development",
+    "databases",
+    "robotics process Automation",
+    "data warehousing and ETL",
+    "digital marketing",
+    "Operating systems",
+    "mobile development",
+    "architecture and design patterns",
+    "blockchain",
+];
+
+const catItemDetailslist = [
+    CloudComputing = [
+        "cloud architect master program",
+        "Microsoft Azure Cloud Engineer Masters Program",
+        "designing microsoft azure architect certification course",
+        "salesforce CRM master certification program",
+        "AWS master program",
+        "microservices certification training course",
+        "AWS developer certification training",
+        "google cloud platform (GCP) certification training course"
+    ],
+    DevOps = [
+        "DevOps certification training Course",
+        "kubernetes certifiction training course : administrator(CKA)",
+        "AWS DevOps Engeneer certification training course",
+        "docker certuficaation training course",
+        "Git certification training course",
+        "ansible certification training course",
+        "jenkins certification training course",
+        "DevOps engeneer master program",
+    ],
+    BIandVisualization = [
+        "business intelligence master program",
+        "microsoft power BI certification training course",
+        "advance MS excel 2016 certification training",
+        "pentaho BI certification training",
+        "QlikView certification training",
+        "business analys master course",
+        "tableau certification training course"
+    ],
+    CyberSecurity = [
+        "cyber security master program",
+        "cyber security course",
+        "CompTIA Security+ Certification Training - SY0-601 Exam ",
+        "certified ethical hacking course CEHv12",
+        "Cyber Security and Ethical Hacking Internship Program"
+    ],
+    datascience = [
+        "data analytics master program",
+        "python certification training course",
+        "statics essentials for analytics",
+        "data analytics with R programing crtificaation training",
+        "data science with R programing crtificaation training",
+        "advance predivtive modelling in R certification training",
+        "analytics for retail banks",
+        "SAS training and certification",
+    ],
+    ProgrammingandFrameworks = [
+        "full stack web developer master program course",
+        "computer science bootcamp program",
+        "java certification training course",
+        "node.js certification training course",
+        "spring framework certification course",
+        "comprehensive java course certification training",
+        "python django certification training course",
+        "python scripting certification training",
+    ],
+    ProjectManagementMethodology = [
+        "PMP® Certification Training Course",
+        "PRINCE2® 6th Edition Foundation & Practitioner Certification Training Course",
+        "Professional Scrum Master Training",
+        "PMI-RMP® Certification Course",
+    ],
+    softwareTesting = [
+        "Selenium Certification Training Course",
+        "Mobile App Testing Using Appium",
+        "Manual Testing Certification Training Course Online",
+        "Software Testing Fundamentals Course",
+        "automation testing engeneer master program",
+    ],
+    bigData = [
+        "Big Data Hadoop Certification Training Course",
+        "Apache Spark and Scala Certification Training Course",
+        "PySpark Certification Training Course",
+        "Apache Kafka Certification Training Course",
+        "Splunk Certification Training: Power User and Admin",
+        "Big Data Hadoop Administration Certification Training",
+    ],
+    artificialIntelligence = [
+        "Artificial Intelligence Certification Course",
+        "ChatGPT-4 Complete Course: Beginners to Advanced",
+        "Machine Learning with Mahout Certification Training",
+        "rienforcement learning",
+        "graphicaal model certification training",
+    ],
+    frontendDevelopment = [
+        "React JS Certification Training Course",
+        "Angular Certification Training Course",
+        "Web Developer Certification Training Course",
+        "HTML Certification Training Course",
+        "CSS Certification Training Course",
+        "JavaScript Certification Training Course",
+        "UI UX Design Certification Course",
+        "jQuery Certification Training Course"
+    ],
+    databases = [
+        "Microsoft SQL Server Certification Course",
+        "MySQL DBA Certification Training",
+        "MongoDB Certification Training Course",
+        "SQL Essentials Training",
+        "Apache Cassandra Certification Training",
+        "Mastering Neo4j Graph Database Certification Training",
+    ],
+    roboticsProcessAutomation = [
+        "RPA using UiPath Certification Training",
+        "Automation Anywhere Certification Training Course",
+        "RPA developer master program"
+    ],
+    dataWarehousingAndETL = [
+        "Informatica Certification Training Course",
+        "Snowflake Certification Training Course",
+        "Talend Certification Training For Big Data Integration",
+        "Data Warehousing and BI Certification Training",
+        "Data Warehousing Certification Training",
+    ],
+    digitalMarketing = [
+        "Digital Marketing Course",
+        "Measuring Social Media ROI",
+        "Mastering Google Analytics",
+    ],
+    operetingSystems = [
+        "Linux Administration Certification Training Course",
+        "Linux Fundamentals Certification Training",
+        "unix shell scripting certification training"
+    ],
+    mobileDevelopment = [
+        "Android Certification Training Course",
+        "iOS App Development Certification Training",
+
+    ],
+    architectureDesignPatterns = [
+        "design patterns certification training",
+
+    ],
+    blockchain = [
+        "blockchain developer certification training",
+        "ethereum developer certification training",
+    ]
+]
+
+const catItemDetailsCard = [
+    CloudComputing = [
+        "master program", "./homepage-img/master-program.png", "cloud architect master program", "12", "250", "31k"
+    ],
+    DevOps = [
+        "live course", "./homepage-img/master-program.png", "DevOps certification training Course", "10", "200", "11k"
+    ],
+    BIandVisualization = [
+        "master program", "./homepage-img/master-program.png", "business intelligence master program", "14", "250", "41k"
+    ],
+    CyberSecurity = [
+        "master program", "./homepage-img/master-program.png", "cyber security master program", "5", "100", "6k"
+    ],
+    datascience = [
+        "master program", "./homepage-img/master-program.png", "data analytics master program", "8", "270", "61k"
+    ],
+    ProgrammingandFrameworks = [
+        "master program", "./homepage-img/master-program.png", "full stack web developer master program course", "7", "80", "11k"
+    ],
+    ProjectManagementMethodology = [
+        "master program", "./homepage-img/master-program.png", "PMP® Certification Training Course", "7", "140", "13k"
+    ],
+    softwareTesting = [
+        "master program", "./homepage-img/master-program.png", "Selenium Certification Training Course", "5", "80", "1k"
+    ],
+    bigData = [
+        "master program", "./homepage-img/master-program.png", "Big Data Hadoop Certification Training Course", "2", "20", "1k"
+    ],
+    artificialIntelligence = [
+        "master program", "./homepage-img/master-program.png", "Artificial Intelligence Certification Course", "12", "250", "31k"
+    ],
+    frontendDevelopment = [
+        "master program", "./homepage-img/master-program.png", "React JS Certification Training Course", "12", "250", "31k"
+    ],
+    databases = [
+        "master program", "./homepage-img/master-program.png", "Microsoft SQL Server Certification Course", "12", "250", "31k"
+    ],
+    roboticsProcessAutomation = [
+        "master program", "./homepage-img/master-program.png", "RPA using UiPath Certification Training", "12", "250", "31k"
+    ],
+    dataWarehousingAndETL = [
+        "master program", "./homepage-img/master-program.png", "Informatica Certification Training Course", "12", "250", "31k"
+    ],
+    digitalMarketing = [
+        "master program", "./homepage-img/master-program.png", "Digital Marketing Course", "12", "250", "31k"
+    ],
+    operetingSystems = [
+        "master program", "./homepage-img/master-program.png", "Linux Administration Certification Training Course", "12", "250", "31k"
+    ],
+    mobileDevelopment = [
+        "master program", "./homepage-img/master-program.png", "Android Certification Training Course", "12", "250", "31k"
+
+    ],
+    architectureDesignPatterns = [
+        "master program", "./homepage-img/master-program.png", "design patterns certification training", "12", "250", "31k"
+    ],
+    blockchain = [
+        "master program", "./homepage-img/master-program.png", "blockchain developer certification training", "12", "250", "31k"
+    ]
+]
--- script.js
+++ script.js
@@ -1,143 +1,208 @@
+// javascript for homepage  advertisement  section
+// function timer() {
+//   const endTime = new Date("26 June 2023 02:00:00 PM");
+//   const currentTime = new Date();
+//   const diffr = (endTime - currentTime) / 1000; 
+//   const days = Math.floor(diffr / (60 * 60 * 24));
+//   const hours = Math.floor(diffr / (60 * 60) % 24);
+//   const minutes = Math.floor((diffr / 60) % 60)
+//   const seconds = Math.floor(diffr % 60)
+//   const showTime = document.getElementsByClassName("time");
+//   showTime[0].innerHTML = days;
+//   showTime[1].innerHTML = hours;
+//   showTime[2].innerHTML = minutes;
+//   showTime[3].innerHTML = seconds;
+// }
+// const adPara = document.querySelector(".ad-para");
+// const cutAdBtn = document.getElementById("cutAd");
+// const viewOffer = document.getElementById("viewOffer");
 
-// javascript for advertisement 
-function timer() {
-  const endTime = new Date("26 June 2023 02:00:00 PM");
-  const currentTime = new Date();
-  const diffr = (endTime - currentTime) / 1000; //time diff is now in seconds
-  /*  
-1 day = 24 hours
-1 hour = 60 minuts
-1 minut = 60 seconds
-*/
-  const days = Math.floor(diffr / (60 * 60 * 24));
-  const hours = Math.floor(diffr / (60 * 60) % 24);
-  const minutes = Math.floor((diffr / 60) % 60)
-  const seconds = Math.floor(diffr % 60)
-  const showTime = document.getElementsByClassName("time");
-  showTime[0].innerHTML = days;
-  showTime[1].innerHTML = hours;
-  showTime[2].innerHTML = minutes;
-  showTime[3].innerHTML = seconds;
-}
-setInterval(() => {
-  timer()
-}, 1000);
+// setTimeout(() => {
+//   setInterval(() => {
+//     timer()
+//   }, 1000);
+//   adPara.classList.remove("hide");
+//   adPara.classList.add("show");
+// }, 2000);
+// cutAdBtn.addEventListener("click", () => {
+//   adPara.classList.remove("show");
+//   adPara.classList.add("hide");
+//   viewOffer.classList.remove("hide")
+//   viewOffer.classList.add("show")
+// })
+
+// viewOffer.addEventListener("click", () => {
+//   adPara.classList.remove("hide");
+//   adPara.classList.add("show");
+//   viewOffer.classList.add("hide");
+//   viewOffer.classList.remove("show");
+// })
 
 
-const adPara = document.querySelector(".ad-para");
-//showAd with delay of 3 secons after pageload
-function setAd() {
-  setTimeout(() => {
-    adPara.classList.add("show");
-    viewOffer.classList.add("hide");
-  }, 1);
-}
-setAd()
-
-
-// to hide ad function starts here
-const cutAdBtn = document.getElementById("cutAd");
-const viewOffer = document.getElementById("viewOffer");
-
-viewOffer.addEventListener("click", () => {
-  adPara.classList.remove("hide");
-  adPara.classList.add("show");
-  viewOffer.classList.add("hide");
-})
-cutAdBtn.addEventListener("click", () => {
-  adPara.classList.remove("show");
-  viewOffer.classList.remove("hide")
-  viewOffer.classList.add("show")
-})
-// mobile navbar
 const browseCatMob = document.querySelector(".browse-cat-con");
 const mobNavCon = document.querySelector(".inner-con");
 const mobileNavBtn = document.querySelector(".mob-nav-btn");
 const browseCatHead = document.querySelector(".brows-cat-heading");
 const shownavBtn = document.getElementById("showNav");
 const hiddenDropdown = document.querySelector(".hidden-dropdown");
+const catItemContainer = document.querySelector(".cat-item-container");
 const catagories = document.querySelector(".categories");
+const catItemDetails = document.querySelector(".cat-item-details");
 const rightSide = document.querySelector(".right-side");
 const leftSide = document.querySelector(".left-side");
 const deskMainNav = document.querySelector(".desktop-main-nav");
+const trendingCoursesBtn = document.querySelector("#showTrendingCourses");
+const trendingCoursesItems = document.querySelector(".trending-couses-items");
+const browseCatBtn = document.querySelector(".browse-categores");
+const catItemCard = document.querySelector(".cat-item-card");
 
-mobileNavBtn.addEventListener("click", () => {
-  browseCatMob.style.display = "block";
-  mobNavCon.style.transform = "translateX(300px)"
-  hiddenDropdown.style.display = "none";
-});
+
 browseCatHead.addEventListener("click", () => {
   browseCatMob.style.display = "none";
   hiddenDropdown.classList.remove("show");
+  hiddenDropdown.style.display = "none";
   rightSide.style.transform = "translateX(300px)";
   rightSide.style.visibility = "hidden";
-  leftSide.style.transform = "none";  
+  leftSide.style.transform = "none";
+  trendingCoursesItems.classList.remove("show");
 });
 
-function goRight(){
+function goRight() {
   leftSide.style.transform = "translateX(-300px)";
   rightSide.style.transform = "none";
   rightSide.style.visibility = "visible";
 };
 
-function goLeft(){
+function goLeft() {
   rightSide.style.transform = "translateX(300px)";
   rightSide.style.visibility = "hidden";
   leftSide.style.transform = "none";
 };
-function openBrowseCat(){
+function openBrowseCat() {
   let width = window.matchMedia("(max-width: 800px)")
-  if(width.matches){
+  if (width.matches) {
     browseCatMob.style.display = "block";
     mobNavCon.style.transform = "translateX(300px)"
-    hiddenDropdown.style.display = "none";
     leftSide.style.transform = "translateX(-300px)";
     rightSide.style.transform = "none";
     rightSide.style.visibility = "visible";
-  } 
+    hiddenDropdown.style.display = "none";
+    hiddenDropdown.classList.remove("show");
+    trendingCoursesItems.classList.remove("show");
+  }
 }
 //  navbaar
 shownavBtn.addEventListener("click", () => {
-  document.querySelector(".hidden-dropdown").classList.toggle("show");
-  if (document.querySelector(".hidden-dropdown").classList.contains("show") == true) {
+  hiddenDropdown.classList.toggle("show");
+  if (hiddenDropdown.classList.contains("show") == true) {
     hiddenDropdown.style.display = "block";
+    trendingCoursesItems.classList.remove("show");
+    catItemContainer.classList.add("hide");
   }
   else {
     hiddenDropdown.style.display = "none";
+    catItemContainer.classList.remove("hide");
   }
 })
+mobileNavBtn.addEventListener("click", () => {
+  browseCatMob.style.display = "block";
+  mobNavCon.style.transform = "translateX(300px)"
+  hiddenDropdown.style.display = "none";
+  hiddenDropdown.classList.remove("show")
+  trendingCoursesItems.classList.remove("show");
+});
 
+showTrendingCourses.addEventListener("click", () => {
+  trendingCoursesItems.classList.toggle("show");
+});
 // parse jason file to populate html to show categories in navbaar
+
+
 fetch('homepage-data.json')
   .then((response) => response.json())
   .then((list) => {
+
     for (let i = 0; i < 8; i++) {
       deskMainNav.innerHTML += `<li><a>${list.categoriesItems[i]}</a></li>`
     }
     for (let i = 0; i < list.categoriesItems.length; i++) {
-      hiddenDropdown.innerHTML += `<li class="hidden-dropdown-items"><a>${list.categoriesItems[i]}</a></li>`
-      catagories.innerHTML += `<li class="categories-li-items"><a>${list.categoriesItems[i]}</a></li>`
-      rightSide.innerHTML += `<li class="right-slide-items"><a>${list.categoriesItems[i]}</a></li>`
+      hiddenDropdown.innerHTML += `<li class="hidden-dropdown-items"><a>${list.categoriesItems[i]}</a></li>`;
+
+      rightSide.innerHTML += `<li class="right-slide-items categories-li-items"><a>${list.categoriesItems[i]}</a></li>`
+      trendingCoursesItems.innerHTML += `<li class="item-list"><a>${list.categoriesItems[i]}</a></li>`
     }
+
   });
 
 
-  // image swiper
-  var swiper = new Swiper(".mySwiper", {
-    speed: 1400,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 2800,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// image swiper
+var swiper = new Swiper(".mySwiper", {
+  speed: 1400,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+for (let i = 0; i < categoriesItems.length; i++) {
+  catagories.innerHTML += `<li class="categories-li-items"><a class="cat-li-items-a">${categoriesItems[i]}</a></li>`
+}
+
+catLi = Array.from(catagories.children)
+catLi.forEach(element => {
+  element.addEventListener("mouseenter", () => {
+    catItemDetails.innerHTML = "";
+    for (let i = 0; i < catItemDetailslist[catLi.indexOf(element)].length; i++) {
+      catItemDetails.innerHTML += `<li>${catItemDetailslist[catLi.indexOf(element)][i]}</li>`;
+    }
+     catItemCard.innerHTML = `<h4>${catItemDetailsCard[catLi.indexOf(element)][0]}</h4>
+<br>
+<div class="dflex mark-title">
+    <img src="${catItemDetailsCard[catLi.indexOf(element)][1]}" alt="">
+    <p>${catItemDetailsCard[catLi.indexOf(element)][2]}</p>
+</div>
+<div class="dflex card-details">
+    <div class="courses">
+        <small>courses</small>
+        <br>
+        <span>${catItemDetailsCard[catLi.indexOf(element)][3]}</span>
+    </div>
+    <div class="hours">
+        <small>hours</small>
+        <br>
+        <span>${catItemDetailsCard[catLi.indexOf(element)][4]}</span>
+    </div>
+    <div class="learners">
+        <small>learners</small>
+        <br>
+        <span>${catItemDetailsCard[catLi.indexOf(element)][5]}</span>
+    </div>
+</div>
+<div class="card-review">
+    <span>review</span>
+    <span>
+        <i class="fa-solid fa-star star-color"></i>
+        <i class="fa-solid fa-star star-color"></i>
+        <i class="fa-solid fa-star star-color"></i>
+        <i class="fa-solid fa-star star-color"></i>
+        <i class="fa-solid fa-star"></i>
+        4(1200)
+    </span>
+</div>
+<button class="card-view-details">View Details</button>`
+  })
+});
+
+
